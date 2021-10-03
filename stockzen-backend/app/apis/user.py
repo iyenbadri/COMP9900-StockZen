056d2ffd3ca08755dc4975695b296aea935ce300
Gunjan Chaudhari <z5302729@unsw.edu.au>
2021-10-03 19:49:15 +1100

    backend logout

--- stockzen-backend/app/apis/user.py
+++ stockzen-backend/app/apis/user.py
@@ -12,7 +12,7 @@ from app import login_manager
 from app.models.schema import User
 from app.utils.enums import AuthStatus
 from flask import request
-from flask_login import current_user
+from flask_login import current_user, logout_user
 from flask_login.utils import login_required, login_user
 from flask_restx import Namespace, Resource, fields, marshal
 
@@ -131,3 +131,11 @@ def user_loader(user_id: int):
 
     """
     return User.query.get(user_id)
+
+
+@api.route("/logout")
+class UserRouter(Resource):
+    @api.response(200, "logged out")
+    def get(self):
+        logout_user()
+        return {"message": "You have succesfully logged out"}, 200
