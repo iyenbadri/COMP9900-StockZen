2471cef54cb4de9b48731177d1c2fbfea2edf5ed
khailiew <61170435+khailiew@users.noreply.github.com>
2021-10-09 20:01:46 +1100

    Feat b/portfolios lots db (#31)
    * Added Portfolio and Stock Lots databases
    
    * Update schema to include required tables and objects
    
    * Implement endpoints for Portfolio and Stock; refactor
    
    * Add placeholders for individual GET endpoints
    
    * Implement individual fetch routes, better typings, refactor db utils, looser coupling, downsize enums, add Stock UniqueConstraint
    
    * Formatting
    
    Co-authored-by: iyenbadri <badrivishaliyengar@gmail.com>

--- stockzen-backend/app/utils/auth_utils.py
+++ stockzen-backend/app/utils/auth_utils.py
@@ -3,42 +3,28 @@ from typing import Dict, Union
 from app.models.schema import User
 
 from . import db_utils as db
-from .enums import AuthStatus
+from .enums import Status
 
 
-def email_exists(email: str) -> AuthStatus:
+def email_exists(email: str) -> Status:
     """Check if user email already exists"""
-    if db.query_user(email):
-        return AuthStatus.USER_FOUND
-    return AuthStatus.USER_NOT_FOUND
+    try:
+        db.query_user(email)
+        return Status.FOUND
+    except:
+        return Status.NOT_FOUND
 
 
-def add_user(
-    email: str, first_name: str, last_name: str, plain_password: str
-) -> AuthStatus:
-    """Add a user to the database, return bool of success status"""
-    new_user = User(
-        email=email,
-        first_name=first_name,
-        last_name=last_name,
-    )
-    new_user.set_password(plain_password)  # carry out hash and save to user object
-
-    if db.insert_user(new_user):
-        return AuthStatus.USER_ADDED
-    return AuthStatus.USER_NOT_ADDED
-
-
-def validate_login(email: str, plain_password: str) -> Union[User, AuthStatus]:
+def validate_login(email: str, plain_password: str) -> Union[User, Status]:
     """Validates user login and returns response message"""
-    user = db.query_user(email)
-
-    if not user:
-        return AuthStatus.USER_NOT_FOUND
-    if user.check_password(plain_password):
-        return user
-    else:
-        return AuthStatus.INCORRECT_PASSWORD
+    try:
+        user = db.query_user(email)
+        if user.check_password(plain_password):
+            return user
+        else:
+            return Status.INCORRECT_PASSWORD
+    except:
+        return Status.NOT_FOUND
 
 
 UserDetails = Dict[str, str]
@@ -46,4 +32,4 @@ UserDetails = Dict[str, str]
 
 def extract_user_details(user: User) -> UserDetails:
     """Takes a User instance and returns user details dict"""
-    return {"firstName": user.first_name, "lastName": user.last_name}
+    return {"first_name": user.first_name, "last_name": user.last_name}
