f1d0f5b434307d6abd2d817c33514b89a559bd3e
Khai Liew <khai.liew@unsw.edu.au>
2021-10-12 21:18:28 +1100

    Implement PortfolioListSummary Component (unstyled)

--- stockzen-frontend/src/components/Portfolio/PortfolioListSummary.tsx
+++ stockzen-frontend/src/components/Portfolio/PortfolioListSummary.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { usdFormatter } from 'utils/Utilities';
+import styles from './PortfolioListSummary.module.css';
 
 const PortfolioListSummary = () => {
-  return <>PortfolioListSummary</>;
+  let portfolioSummary = {
+    holding: 50210.4,
+    todayChangePercent: 0.82,
+    overallChangePercent: -10.76,
+  };
+  return (
+    <>
+      <div className={styles.summaryContainer}>
+        <div className={styles.summaryWrapper}>
+          <div className={styles.summaryTitle}>Today</div>
+          <div className={`${styles.summaryValue} outerStroke`}>
+            {portfolioSummary.todayChangePercent}%
+          </div>
+        </div>
+        <div className={styles.summaryWrapper}>
+          <div className={styles.summaryTitle}>My Holdings</div>
+          <div className={`${styles.summaryValue} outerStroke`}>
+            {usdFormatter.format(portfolioSummary.holding)}
+          </div>
+        </div>
+
+        <div className={styles.summaryWrapper}>
+          <div className={styles.summaryTitle}>Overall</div>
+          <div className={`${styles.summaryValue} outerStroke`}>
+            {portfolioSummary.overallChangePercent}%
+          </div>
+          <hr className={styles.separatorLine} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PortfolioListSummary;
