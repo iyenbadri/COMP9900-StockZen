dd4700d25f956c44d4152568f86d5d51750a7c7f
Khai Liew <khai.liew@unsw.edu.au>
2021-10-12 23:25:33 +1100

    Implement styles, refactor

--- stockzen-frontend/src/components/Portfolio/PortfolioListSummary.tsx
+++ stockzen-frontend/src/components/Portfolio/PortfolioListSummary.tsx
@@ -1,37 +1,47 @@
+import lossArrow from 'assets/icon-outlines/outline-arrow-down-circle-red.svg';
+import gainArrow from 'assets/icon-outlines/outline-arrow-up-circle-green.svg';
 import React from 'react';
 import { usdFormatter } from 'utils/Utilities';
 import styles from './PortfolioListSummary.module.css';
-
 const PortfolioListSummary = () => {
-  let portfolioSummary = {
-    holding: 50210.4,
-    todayChangePercent: 0.82,
-    overallChangePercent: -10.76,
+  const summaryData = {
+    holdings: 7248.1,
+    todayChangePercent: 10.15,
+    overallChangePercent: -0.79,
   };
+
+  // does not show any img if no change
+  const gainLossArrow = (change: number) => {
+    if (change > 0) return <img src={gainArrow} alt='up green arrow' />;
+    if (change < 0) return <img src={lossArrow} alt='down red arrow' />;
+  };
+
   return (
     <>
       <div className={styles.summaryContainer}>
         <div className={styles.summaryWrapper}>
           <div className={styles.summaryTitle}>Today</div>
           <div className={`${styles.summaryValue} outerStroke`}>
-            {portfolioSummary.todayChangePercent}%
+            {gainLossArrow(summaryData.todayChangePercent)}
+            <span>{summaryData.todayChangePercent}%</span>
           </div>
         </div>
         <div className={styles.summaryWrapper}>
           <div className={styles.summaryTitle}>My Holdings</div>
           <div className={`${styles.summaryValue} outerStroke`}>
-            {usdFormatter.format(portfolioSummary.holding)}
+            <span> {usdFormatter.format(summaryData.holdings)}</span>
           </div>
         </div>
 
         <div className={styles.summaryWrapper}>
           <div className={styles.summaryTitle}>Overall</div>
           <div className={`${styles.summaryValue} outerStroke`}>
-            {portfolioSummary.overallChangePercent}%
+            {gainLossArrow(summaryData.overallChangePercent)}
+            <span>{summaryData.overallChangePercent}%</span>
           </div>
-          <hr className={styles.separatorLine} />
         </div>
       </div>
+      <hr className={styles.separatorLine} />
     </>
   );
 };
