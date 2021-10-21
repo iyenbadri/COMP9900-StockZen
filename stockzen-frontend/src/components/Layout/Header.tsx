779be45d320a5f1291a72e20cc22eb494f30a9af
jeehyunn <74037066+jeehyunn@users.noreply.github.com>
2021-10-21 16:29:17 +0900

    Add tabs in header at stock page

--- stockzen-frontend/src/components/Layout/Header.tsx
+++ stockzen-frontend/src/components/Layout/Header.tsx
@@ -2,14 +2,16 @@ import logo_dark from 'assets/stockzen_dark_cropped.png';
 import logo_light from 'assets/stockzen_light_cropped.png';
 import { UserContext } from 'contexts/UserContext';
 import React, { useContext } from 'react';
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
   const { isAuthenticated } = useContext(UserContext);
+  const location = useLocation();
 
   return (
     <header className={isAuthenticated ? styles.userAuthenticated : ''}>
@@ -21,6 +23,24 @@ const Header = () => {
               {!isAuthenticated && <img src={logo_dark} alt='StockZen' />}
             </Link>
           </Col>
+          {location.pathname === '/search' && (
+            <Col xs={6}>
+              <Link to="/portfolio">
+                <Button
+                  variant={'transparent'}
+                  className={styles.tabs}
+                >
+                  My Portfolios
+                </Button>
+              </Link>
+              <Button
+                variant={'transparent'}
+                className={styles.tabs}
+              >
+                Challenge
+              </Button>
+            </Col>
+          )}
           <Col className='text-end'>
             {!isAuthenticated && (
               <>
