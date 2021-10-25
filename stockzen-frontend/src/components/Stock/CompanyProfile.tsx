4123c4cf3418ac503e75e6302c5310aa1bcaee84
jeehyunn <74037066+jeehyunn@users.noreply.github.com>
2021-10-25 17:11:15 +0900

    Add modules for each tab in stock info table

---
+++ stockzen-frontend/src/components/Stock/CompanyProfile.tsx
@@ -0,0 +1,7 @@
+const CompanyProfile = () => {
+  return (
+    <h3>Company related info</h3>
+  );
+};
+
+export default CompanyProfile;
--- stockzen-frontend/src/components/Stock/StockPage.tsx
+++ stockzen-frontend/src/components/Stock/StockPage.tsx
@@ -2,25 +2,45 @@ import refreshIcon from 'assets/icon-outlines/outline-refresh-small.svg';
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import CompanyProfile from './CompanyProfile';
+import StockHistory from './StockHistory';
 import styles from './StockPage.module.css';
-
-// interface Stockdata {
-//   stockId: number;
-//   symbol: string;
-//   name: string;
-//   price: number;
-//   change: number;
-//   changePercent: number;
-//   averagePrice: number;
-//   profit: number;
-//   profitPercent: number;
-//   value: number;
-// }
+import StockSummary from './StockSummary';
 
 const StockPage = () => {
   const reloadStockInfo = () => {
-    // TO DO: API wireup 
+    // TO DO: API wire up 
     window.location.reload();
+  }
+
+  // TO DO: API wire up
+  const stockData: IStockInfo = {
+    stockId: 0,
+    symbol: 'GOOG',
+    name: 'Alphabet Inc.',
+    price: 214.21,
+    change: -0.09,
+    changePercent: 0.08,
+    avgPrice: 252.40,
+    profit: 203.10,
+    profitPercent: 0.89,
+    value: 2001.65,
+    summary: {
+      prevClose: 2848.3,
+      open: 2843.84,
+      bid: 0,
+      bidSize: 900,
+      ask: 0,
+      askSize: 800,
+      dayHigh: 2856.99,
+      dayLow: 2832.74,
+      fiftyTwoWeekHigh: 2936.41,
+      fiftyTwoWeekLow: 1514.62,
+      volume: 742496,
+      avgVolume: 1064861,
+      marketCap: 1899503288320,
+      beta: 1.025925,
+    }
   }
 
   const gainLossClass = (change: number): string => {
@@ -37,20 +57,20 @@ const StockPage = () => {
     <>
       {/* <PortfolioListSummary></PortfolioListSummary> */}
       <div className={styles.stockHeader}>
-        <h5 className={styles.stockName}>Alphabet Inc.</h5>
-        <div className={styles.stockCode}>(GOOG)</div>
+        <h5 className={styles.stockName}>{stockData.name}</h5>
+        <div className={styles.stockCode}>({stockData.symbol})</div>
       </div>
       <div className={styles.stockSummmary}>
-        <div className={`${styles.stockPrice} outerStroke`}> 2,823.66</div>
+        <div className={`${styles.stockPrice} outerStroke`}>{stockData.price}</div>
         <div
-          className={`${styles.stockChange} ${gainLossClass(-0.09)}`}
+          className={`${styles.stockChange} ${gainLossClass(stockData.change)}`}
         >
-          -0.09
+          {stockData.change}
         </div>
         <div
-          className={`${styles.stockPercChange} ${gainLossClass(-0.09)}`}
+          className={`${styles.stockPercChange} ${gainLossClass(stockData.change)}`}
         >
-          (0.08%)
+          ({stockData.changePercent})
         </div>
         <div className={styles.update}>
           <Button
@@ -92,13 +112,13 @@ const StockPage = () => {
           </Tab>
         </TabList>
         <TabPanel>
-          <h4>Summary Content</h4>
+          <StockSummary {...stockData.summary} />
         </TabPanel>
         <TabPanel>
-          <h4>Historical Data Content</h4>
+          <StockHistory />
         </TabPanel>
         <TabPanel>
-          <h4>Company Related Content</h4>
+          <CompanyProfile />
         </TabPanel>
       </Tabs>
     </>
