d5ee80df2e2465a8961339afc26bbf61e34757b6
jeehyunn <74037066+jeehyunn@users.noreply.github.com>
2021-10-26 13:15:55 +0900

    Implement company profile tab

--- stockzen-frontend/src/components/Stock/CompanyProfile.tsx
+++ stockzen-frontend/src/components/Stock/CompanyProfile.tsx
@@ -1,6 +1,30 @@
-const CompanyProfile = () => {
+import linkIcon from 'assets/icon-outlines/outline-link.svg';
+import React, { FC } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import styles from './CompanyProfile.module.css';
+
+const CompanyProfile: FC<IStockProfile> = (prop) => {
   return (
-    <h3>Company related info</h3>
+    <>
+      <div className={styles.profileWrapper}>
+        <div>
+          <div className={styles.companyName}>{prop.name}</div>
+          <a
+            className={styles.link}
+            href={prop.website === null ? '-' : prop.website}
+            target='_blank'>
+            <img className={styles.linkImg} src={linkIcon} alt='website link'></img>
+          </a>
+        </div>
+        <Row className={`mb-2`}>
+          <Col><b>Industry: </b>{prop.industry}</Col>
+          <Col><b>Sector: </b>{prop.sector}</Col>
+        </Row>
+        <div className={`mb-1`}><b>Business Summary</b></div>
+        <div className={`${styles.description}`}>{prop.longBusinessSummary}</div>
+      </div>
+
+    </>
   );
 };
 
