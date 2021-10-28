923755bf1aebfc7efb0360e78dacc5c34ff637f9
jeehyunn <74037066+jeehyunn@users.noreply.github.com>
2021-10-28 13:38:47 +0900

    Change schema of stock page response

--- stockzen-frontend/src/components/Stock/StockPage.tsx
+++ stockzen-frontend/src/components/Stock/StockPage.tsx
@@ -14,40 +14,35 @@ const StockPage = () => {
   }
 
   // TO DO: API wire up
-  const stockData: IStockInfo = {
-    stockId: 0,
-    symbol: 'GOOG',
-    name: 'Alphabet Inc.',
+  const stockData: IStockPageResponse = {
+    id: 0,
+    code: 'GOOG',
+    stockName: 'Alphabet Inc.',
+    exchange: '-',
     price: 214.21,
     change: -0.09,
-    changePercent: 0.08,
-    avgPrice: 252.40,
-    profit: 203.10,
-    profitPercent: 0.89,
-    value: 2001.65,
-    summary: {
-      prevClose: 2848.3,
-      open: 2843.84,
-      bid: 0,
-      bidSize: 900,
-      ask: 0,
-      askSize: 800,
-      dayHigh: 2856.99,
-      dayLow: 2832.74,
-      fiftyTwoWeekHigh: 2936.41,
-      fiftyTwoWeekLow: 1514.62,
-      volume: 742496,
-      avgVolume: 1064861,
-      marketCap: 1899503288320,
-      beta: 1.025925,
-    },
-    profile: {
-      name: 'Alphabet Inc.',
-      industry: 'Internet Content & Information',
-      sector: 'Communication Services',
-      website: 'http://www.abc.xyz',
-      longBusinessSummary: "Alphabet Inc. provides online advertising services in the United States, Europe, the Middle East, Africa, the Asia-Pacific, Canada, and Latin America. The company offers performance and brand advertising services. It operates through Google Services, Google Cloud, and Other Bets segments. The Google Services segment provides products and services, such as ads, Android, Chrome, hardware, Google Maps, Google Play, Search, and YouTube, as well as technical infrastructure; and digital content. The Google Cloud segment offers infrastructure and data analytics platforms, collaboration tools, and other services for enterprise customers. The Other Bets segment sells internet and TV services, as well as licensing and research and development services. The company was founded in 1998 and is headquartered in Mountain View, California.",
-    }
+    percChange: 0.08,
+    prevClose: 2848.3,
+    open: 2843.84,
+    bid: 0,
+    bidSize: 900,
+    ask: 0,
+    askSize: 800,
+    dayHigh: 2856.99,
+    dayLow: 2832.74,
+    fiftyTwoWeekHigh: 2936.41,
+    fiftyTwoWeekLow: 1514.62,
+    volume: 742496,
+    avgVolume: 1064861,
+    marketCap: 1899503288320,
+    beta: 1.025925,
+    longName: 'Alphabet Inc.',
+    industry: 'Internet Content & Information',
+    sector: 'Communication Services',
+    website: 'http://www.abc.xyz',
+    longBusinessSummary: "Alphabet Inc. provides online advertising services in the United States, Europe, the Middle East, Africa, the Asia-Pacific, Canada, and Latin America. The company offers performance and brand advertising services. It operates through Google Services, Google Cloud, and Other Bets segments. The Google Services segment provides products and services, such as ads, Android, Chrome, hardware, Google Maps, Google Play, Search, and YouTube, as well as technical infrastructure; and digital content. The Google Cloud segment offers infrastructure and data analytics platforms, collaboration tools, and other services for enterprise customers. The Other Bets segment sells internet and TV services, as well as licensing and research and development services. The company was founded in 1998 and is headquartered in Mountain View, California.",
+    prediction: 0,
+    confidence: 0
   }
 
   const gainLossClass = (change: number): string => {
@@ -64,8 +59,8 @@ const StockPage = () => {
     <>
       {/* <PortfolioListSummary></PortfolioListSummary> */}
       <div className={styles.stockHeader}>
-        <h5 className={styles.stockName}>{stockData.name}</h5>
-        <div className={styles.stockCode}>({stockData.symbol})</div>
+        <h5 className={styles.stockName}>{stockData.stockName}</h5>
+        <div className={styles.stockCode}>({stockData.code})</div>
       </div>
       <div className={styles.stockSummmary}>
         <div className={`${styles.stockPrice} outerStroke`}>{stockData.price}</div>
@@ -77,7 +72,7 @@ const StockPage = () => {
         <div
           className={`${styles.stockPercChange} ${gainLossClass(stockData.change)}`}
         >
-          ({stockData.changePercent})
+          ({stockData.percChange})
         </div>
         <div className={styles.update}>
           <Button
@@ -119,13 +114,13 @@ const StockPage = () => {
           </Tab>
         </TabList>
         <TabPanel>
-          <StockSummary {...stockData.summary} />
+          <StockSummary {...stockData} />
         </TabPanel>
         <TabPanel>
           <StockHistory />
         </TabPanel>
         <TabPanel>
-          <CompanyProfile {...stockData.profile} />
+          <CompanyProfile {...stockData} />
         </TabPanel>
       </Tabs>
     </>
