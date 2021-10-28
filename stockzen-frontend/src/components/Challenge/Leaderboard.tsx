6a53fa8c4156c5826524f780cbbec90167227c4b
Rittisak Kwanda <rittisak.kwanda@gmail.com>
2021-10-28 23:08:31 +1100

    Mock up the challenge page

--- stockzen-frontend/src/components/Challenge/Leaderboard.tsx
+++ stockzen-frontend/src/components/Challenge/Leaderboard.tsx
@@ -3,6 +3,7 @@ import React, { useContext, useEffect } from 'react';
 import refresh from 'assets/icon-outlines/outline-refresh-small.svg';
 import Button from 'react-bootstrap/Button';
 import styles from './Leaderboard.module.css';
+import varticalDot from 'assets/icon-outlines/outline-menu-vertical.svg';
 
 const Leaderboard = () => {
   // Get the setShowPortfolioSummary from context
@@ -18,10 +19,10 @@ const Leaderboard = () => {
 
   return (
     <>
-      <h2 className='text-center'>Portfolio Challenge</h2>
+      <h2 className={styles.pageHeader}>Portfolio Challenge</h2>
       <div className={styles.contentPadder}>
         <div>
-          Leaderboard{' '}
+          <span className={styles.leaderboardTitle}>Leaderboard</span>{' '}
           <Button
             variant='light'
             className='ms-1 text-muted d-flex-inline align-items-center'
@@ -31,18 +32,95 @@ const Leaderboard = () => {
           </Button>
         </div>
 
-        <div>
+        <div className={styles.challengeDateMessage}>
           Current challenge: 00:00{' '}
           <span className={styles.challengeDate}>05/09/2021</span> - 23:59{' '}
           <span className={styles.challengeDate}>18/09/2021</span>
         </div>
 
-        <div>Leaderboard table</div>
+        <div className={styles.leaderboardTable}>
+          <div className={styles.leaderboardTableHeader}>
+            <div className={styles.rowYou}></div>
+            <div className={styles.rowRank}>Rank</div>
+            <div className={styles.rowUser}>User</div>
+            <div className={styles.rowGain}>Portfolio Gain</div>
+            <div className={styles.rowTopStock}>Top Stock</div>
+          </div>
+        </div>
+
+        <div className={styles.leaderboardTableRow}>
+          <div className={styles.rowYou}></div>
+          <div className={`${styles.rowInfo} ${styles.rank1}`}>
+            <div className={styles.rowRank}>1</div>
+            <div className={styles.rowUser}>User Name</div>
+            <div className={styles.rowGain}>32.05%</div>
+            <div className={styles.rowTopStock}>GOOG</div>
+          </div>
+        </div>
+
+        <div className={styles.leaderboardTableRow}>
+          <div className={styles.rowYou}></div>
+          <div className={`${styles.rowInfo} ${styles.rank2}`}>
+            <div className={styles.rowRank}>2</div>
+            <div className={styles.rowUser}>User Name</div>
+            <div className={styles.rowGain}>25.10%</div>
+            <div className={styles.rowTopStock}>TSLA</div>
+          </div>
+        </div>
+
+        <div className={styles.leaderboardTableRow}>
+          <div className={styles.rowYou}></div>
+          <div className={`${styles.rowInfo} ${styles.rank3}`}>
+            <div className={styles.rowRank}>3</div>
+            <div className={styles.rowUser}>User Name</div>
+            <div className={styles.rowGain}>10.10%</div>
+            <div className={styles.rowTopStock}>ABC</div>
+          </div>
+        </div>
+
+        <div className={styles.leaderboardTableRow}>
+          <div className={styles.rowYou}></div>
+          <div className={styles.rowInfo}>
+            <div className={styles.rowRank}>4</div>
+            <div className={styles.rowUser}>User Name</div>
+            <div className={styles.rowGain}>10.10%</div>
+            <div className={styles.rowTopStock}>ABC</div>
+          </div>
+        </div>
+
+        <div className={styles.leaderboardTableRow}>
+          <div className={styles.rowYou}></div>
+          <div className={styles.rowInfo}>
+            <div className={styles.rowRank}>5</div>
+            <div className={styles.rowUser}>User Name</div>
+            <div className={styles.rowGain}>10.10%</div>
+            <div className={styles.rowTopStock}>ABC</div>
+          </div>
+        </div>
+
+        <div className={styles.leaderboardTableRow}>
+          <div className={styles.rowYou}></div>
+          <div className={styles.rowMore}>
+            <img src={varticalDot} alt='more' />
+          </div>
+        </div>
+
+        <div className={styles.leaderboardTableRow}>
+          <div className={styles.rowYou}>You</div>
+          <div className={styles.rowInfo}>
+            <div className={styles.rowRank}>1,289</div>
+            <div className={styles.rowUser}>User Name</div>
+            <div className={styles.rowGain}>3.10%</div>
+            <div className={styles.rowTopStock}>XYZ</div>
+          </div>
+        </div>
       </div>
       <hr />
 
       <div className={styles.contentPadder}>
-        <div>
+        <div
+          className={`${styles.challengeDateMessage} ${styles.nextChallengeMessage}`}
+        >
           Next challenge: 00:00{' '}
           <span className={styles.challengeDate}>05/09/2021</span>- 23:59{' '}
           <span className={styles.challengeDate}>18/09/2021</span>
