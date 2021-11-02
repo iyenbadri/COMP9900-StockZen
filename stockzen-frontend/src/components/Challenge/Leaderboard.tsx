c53f2f2d3331be24f9a06e35fbabff23c0d56539
Rittisak Kwanda <rittisak.kwanda@gmail.com>
2021-11-02 20:15:47 +1100

    Add medal icons

--- stockzen-frontend/src/components/Challenge/Leaderboard.tsx
+++ stockzen-frontend/src/components/Challenge/Leaderboard.tsx
@@ -4,6 +4,9 @@ import refresh from 'assets/icon-outlines/outline-refresh-small.svg';
 import Button from 'react-bootstrap/Button';
 import styles from './Leaderboard.module.css';
 import varticalDot from 'assets/icon-outlines/outline-menu-vertical.svg';
+import medal1 from 'assets/medal_1.png';
+import medal2 from 'assets/medal_2.png';
+import medal3 from 'assets/medal_3.png';
 
 const Leaderboard = () => {
   // Get the setShowPortfolioSummary from context
@@ -34,8 +37,8 @@ const Leaderboard = () => {
 
         <div className={styles.challengeDateMessage}>
           Current challenge: 00:00{' '}
-          <span className={styles.challengeDate}>05/09/2021</span> - 23:59{' '}
-          <span className={styles.challengeDate}>18/09/2021</span>
+          <span className={styles.challengeDate}>31/10/2021</span> - 23:59{' '}
+          <span className={styles.challengeDate}>13/11/2021</span>
         </div>
 
         <div className={styles.leaderboardTable}>
@@ -51,7 +54,9 @@ const Leaderboard = () => {
         <div className={styles.leaderboardTableRow}>
           <div className={styles.rowYou}></div>
           <div className={`${styles.rowInfo} ${styles.rank1}`}>
-            <div className={styles.rowRank}>1</div>
+            <div className={styles.rowRank}>
+              <img src={medal1} alt='1' height='35' />
+            </div>
             <div className={styles.rowUser}>User Name</div>
             <div className={styles.rowGain}>32.05%</div>
             <div className={styles.rowTopStock}>GOOG</div>
@@ -61,7 +66,9 @@ const Leaderboard = () => {
         <div className={styles.leaderboardTableRow}>
           <div className={styles.rowYou}></div>
           <div className={`${styles.rowInfo} ${styles.rank2}`}>
-            <div className={styles.rowRank}>2</div>
+            <div className={styles.rowRank}>
+              <img src={medal2} alt='2' height='35' />
+            </div>
             <div className={styles.rowUser}>User Name</div>
             <div className={styles.rowGain}>25.10%</div>
             <div className={styles.rowTopStock}>TSLA</div>
@@ -71,7 +78,9 @@ const Leaderboard = () => {
         <div className={styles.leaderboardTableRow}>
           <div className={styles.rowYou}></div>
           <div className={`${styles.rowInfo} ${styles.rank3}`}>
-            <div className={styles.rowRank}>3</div>
+            <div className={styles.rowRank}>
+              <img src={medal3} alt='3' height='35' />
+            </div>
             <div className={styles.rowUser}>User Name</div>
             <div className={styles.rowGain}>10.10%</div>
             <div className={styles.rowTopStock}>ABC</div>
@@ -122,8 +131,8 @@ const Leaderboard = () => {
           className={`${styles.challengeDateMessage} ${styles.nextChallengeMessage}`}
         >
           Next challenge: 00:00{' '}
-          <span className={styles.challengeDate}>19/09/2021</span> - 23:59{' '}
-          <span className={styles.challengeDate}>02/10/2021</span>
+          <span className={styles.challengeDate}>14/11/2021</span> - 23:59{' '}
+          <span className={styles.challengeDate}>27/11/2021</span>
         </div>
 
         <div>
