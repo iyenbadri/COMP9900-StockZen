05a6746efdfd72bfcc80976ca0ff27b6160dc155
Khai Liew <khai.liew@unsw.edu.au>
2021-11-04 01:13:45 +1100

    Update flask-executor to propagate exceptions

--- stockzen-backend/app/config.py
+++ stockzen-backend/app/config.py
@@ -4,5 +4,6 @@ SQLALCHEMY_DATABASE_URI = f"sqlite:///{APP_DB_PATH}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False  # silence deprecation warning
 EXECUTOR_TYPE = "thread"  # flask-executor thread or processes
 EXECUTOR_MAX_WORKERS = 8  # max concurrents
+EXECUTOR_PROPAGATE_EXCEPTIONS = True  # don't swallow exceptions
 SEARCH_LIMIT = 30
 UPDATE_MIN_INTERVAL = 90  # min seconds before a stock page is considered stale
