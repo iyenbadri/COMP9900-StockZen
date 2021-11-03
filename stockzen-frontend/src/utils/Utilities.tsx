4e68edff0380ad35ba590ef775cda174e23a3a05
Khai Liew <khai.liew@unsw.edu.au>
2021-11-03 15:46:29 +1100

    Bugfix for PortfolioListRow display

--- stockzen-frontend/src/utils/Utilities.tsx
+++ stockzen-frontend/src/utils/Utilities.tsx
@@ -2,3 +2,9 @@ export const usdFormatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
 });
+
+export const percFormatter = new Intl.NumberFormat('en-US', {
+  style: 'decimal',
+  maximumFractionDigits: 2,
+  minimumFractionDigits: 2,
+});
