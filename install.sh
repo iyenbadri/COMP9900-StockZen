33036da59299cc2126410324c3f93d95d807e231
Rittisak Kwanda <rittisak.kwanda@gmail.com>
2021-11-18 19:25:04 +1100

    Skip the confirmation in the script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install python3 python3-venv nodejs npm
+sudo apt-get install -y python3 python3-venv nodejs npm
 
 cd stockzen-backend
 python3 -m venv .venv
