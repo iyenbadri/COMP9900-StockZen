2af4fe56382fc7dc7ef39bf7a520deb419c98243
Khai Liew <khai.liew@unsw.edu.au>
2021-11-18 16:00:30 +1100

    Update db_populator for demo challenges

--- stockzen-backend/app/scripts/db_populator.py
+++ stockzen-backend/app/scripts/db_populator.py
@@ -1,10 +1,11 @@
 import os
 from datetime import datetime, timedelta
-from random import randrange, uniform
+from random import randrange, sample, uniform
 
 import app.utils.crud_utils as crud
 import pandas as pd
 from app import create_app, db
+from app.config import TOP_COMPANIES
 from app.models.schema import (
     Challenge,
     ChallengeEntry,
@@ -12,10 +13,11 @@ from app.models.schema import (
     LotSold,
     Portfolio,
     Stock,
+    StockPage,
 )
 from app.utils import db_utils
 from app.utils.enums import Status
-from app.utils.utils import id_to_code
+from app.utils.utils import bulk_challenge_fetch, debug_exception, id_to_code
 from dateutil.parser import parse
 from faker import Faker
 
@@ -34,7 +36,7 @@ def generate_dummy_users(n_users: int):
     """
     generic_password = "Password1!"
 
-    for _ in range(n_users):
+    for _ in range(n_users - 1):
         email = faker.ascii_safe_email()
         first_name = faker.first_name()
         last_name = faker.last_name()
@@ -42,6 +44,15 @@ def generate_dummy_users(n_users: int):
             print(f"Could not add dummy user: {email}, {first_name}, {last_name}")
         else:
             print("Dummy user added")
+    # Add last one with demo credentials
+    email = "demo@demo.com"
+    first_name = "Demo"
+    last_name = "User"
+    if crud.add_user(email, first_name, last_name, generic_password) == Status.FAIL:
+        print(f"Could not add demo user: {email}, {first_name}, {last_name}")
+    else:
+        print("Demo user added")
+
     return email, generic_password
 
 
@@ -79,7 +90,7 @@ def generate_dummy_portfolios(n_portfolios: int, user_id: int):
 
 
 def generate_dummy_stocks(n_stocks: int, n_portfolios: int, user_id: int):
-    STOCK_PAGE_RANGE = 7567
+    STOCK_PAGE_RANGE = 5760
 
     for stock_id in range(1, n_stocks + 1):
         new_stock = Stock(
@@ -146,40 +157,99 @@ def generate_dummy_lots(n_lots: int, n_stocks: int, user_id: int):
 
 def generate_dummy_challenges(n_users):
     try:
-        # Create some challenges
-        for _ in range(n_users - 1):
-            challenge = Challenge(
-                start_date=datetime.now() - timedelta(weeks=2),
-                is_active=False,
-                is_open=True,
-            )
-            db_utils.insert_item(challenge)
-            print("Challenge added")
+        N_CHALLENGES = 2
+
+        # convert top companies to stock_page_ids
+        def code_to_id(code):
+            return StockPage.query.filter_by(code=code).first().id
+
+        top_ids = [code_to_id(company) for company in TOP_COMPANIES]
+
+        # Create closing challenge
+        challenge = Challenge(
+            start_date=datetime.now() - timedelta(weeks=2),
+            is_active=True,
+            is_open=False,
+        )
+        db_utils.insert_item(challenge)
+        print("Challenge added")
+        #  custom active/open challenge
         challenge = Challenge(
             start_date=datetime.now() - timedelta(weeks=1), is_active=True, is_open=True
         )
         db_utils.insert_item(challenge)
         print("Challenge added")
 
-        # Add entries to challenges
-        for i_users in range(1, n_users + 1):
-            for i in range(1, 6):
+        # Add entries to FIRST challenge only
+        # Only do for the last 5 users (or n_users whichever smaller)
+        min = n_users - 5 + 1
+        if min < 1:
+            min = 1
+        for i_users in range(min, n_users + 1):
+            random_ids = sample(top_ids, 5)  # select 5 at random
+            for i in range(1, 6):  # 5 companies per entry
+                chosen_id = random_ids[i - 1]
                 entry = ChallengeEntry(
-                    challenge_id=n_users,
+                    challenge_id=N_CHALLENGES - 1,
                     user_id=i_users,
-                    stock_page_id=i + i_users,  # for stock variety
-                    code=id_to_code(i),
-                    start_price=None,
-                    end_price=None,
-                    perc_change=None,
+                    stock_page_id=chosen_id,
+                    code=id_to_code(chosen_id),
                 )
                 db_utils.insert_item(entry)
                 print("Challenge Entry added")
+        # Update start, end, perc_change
+        bulk_challenge_fetch()
+
+        active = Challenge.query.filter_by(id=1).first()  # manually set to inactive
+        active.is_active = False
+        db.session.commit()
     except Exception as e:
-        print(e)
+        debug_exception(e, suppress=True)
 
 
-def generate_dummy_data(n_users=3, n_portfolios=2, n_stocks=4, n_lots=5):
+# For DEMO
+def populate_demo_user(user_id, portfolio_id, stock_id):
+    try:
+        db_utils.insert_item(Portfolio(user_id=user_id, portfolio_name="My Portfolio 1"))
+        db_utils.insert_item(
+            Stock(user_id=user_id, portfolio_id=portfolio_id, stock_page_id=13)
+        )
+        db_utils.insert_item(
+            LotBought(
+                user_id=user_id,
+                stock_id=stock_id,
+                trade_date=datetime.now(),
+                units=100,
+                unit_price=150.00,
+            )
+        )
+        db_utils.insert_item(
+            LotSold(
+                user_id=user_id,
+                stock_id=stock_id,
+                trade_date=datetime.now(),
+                units=50,
+                unit_price=155.00,
+            )
+        )
+
+        db_utils.insert_item(
+            Stock(user_id=user_id, portfolio_id=portfolio_id, stock_page_id=3644)
+        )
+        db_utils.insert_item(
+            LotBought(
+                user_id=user_id,
+                stock_id=stock_id + 1,
+                trade_date=datetime.now(),
+                units=40,
+                unit_price=700.00,
+            )
+        )
+    except Exception as e:
+        debug_exception(e, suppress=True)
+
+
+def generate_dummy_data(n_users=4, n_portfolios=2, n_stocks=4, n_lots=5):
     """
     Generates dummy user data
     """
@@ -188,10 +258,16 @@ def generate_dummy_data(n_users=3, n_portfolios=2, n_stocks=4, n_lots=5):
 
     # PORTFOLIOS
 
-    for user_id in range(1, n_users + 1):
+    for user_id in range(1, n_users):  # skip demo user
         generate_dummy_portfolios(n_portfolios, user_id)
         generate_dummy_stocks(n_stocks, n_portfolios, user_id)
         generate_dummy_lots(n_lots, n_stocks, user_id)
+    # Demo user special case
+    demo_id = n_users
+    populate_demo_user(
+        demo_id, (demo_id - 1) * n_portfolios + 1, (demo_id - 1) * n_stocks + 1
+    )
+
     generate_dummy_challenges(n_users)
 
     print(
